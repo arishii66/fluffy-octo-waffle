fdb6f43846d6a19ac1884a1fc1aad2c1f46b1210
Asa <ariishi66@gmail.com>
2025-05-11 13:55:00 +0200

    A walking square.

---
+++ src/main/GamePanel.java
@@ -0,0 +1,99 @@
+package main;
+import java.awt.*;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+    //screen settings
+
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; //48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12; //4:3 ratio
+    final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    final int screenHeight = tileSize * maxScreenRow; //576 pixels
+    final int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    //set player default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4; //zooomies
+
+
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000/FPS; //= 1s/fps as nanoseconds = around 0.001666666s
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null){
+
+
+            //updates info: CharPos, DrawScreen w new Info
+            update();
+            repaint(); //calls paintComponent
+
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000; //Nanosec to millisec
+
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    public void update(){
+        if (keyH.upPressed){
+            playerY -= playerSpeed;
+        }
+        if (keyH.downPressed){
+            playerY += playerSpeed;
+        }
+        if (keyH.leftPressed){
+            playerX -= playerSpeed;
+        }
+        if (keyH.rightPressed){
+            playerX += playerSpeed;
+        }
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
+    }
+}
