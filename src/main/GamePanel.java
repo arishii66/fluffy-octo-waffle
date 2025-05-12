55a7f6dd3ccbed9021e0fedfac51d8a173ee0e50
Asa <ariishi66@gmail.com>
2025-05-12 20:20:58 +0200

    MORE TILESSSSSSSSSs

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -13,10 +13,10 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; //48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12; //4:3 ratio
-    final int screenWidth = tileSize * maxScreenCol; //768 pixels
-    final int screenHeight = tileSize * maxScreenRow; //576 pixels
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12; //4:3 ratio
+    public final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; //576 pixels
     final int FPS = 60;
 
 
